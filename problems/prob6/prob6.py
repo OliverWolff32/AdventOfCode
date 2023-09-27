c9c8c10358bd1b58ed530671d76673a48f52242b
Oliver Wolff <oliverwolff@Olivers-MacBook-Air-2.local>
2023-09-27 11:08:01 -0700

    finished prob6

--- problems/prob6/prob6.py
+++ problems/prob6/prob6.py
@@ -1,23 +1,43 @@
-last_four = []
 
-def main():
+
+def part1():
+    last_four = []
     with open("prob6input.txt") as file: 
         string = file.read().strip()
         for i in range(0,len(string)-1): 
             c = string[i]
             if(len(last_four)!= 4):
-                if c not in last_four: 
+                last_four.append(c)
+            elif has_duplicates(last_four): 
+                last_four = cycleArray(last_four, c)
+                if i < 10 : 
+                    print(last_four)
+                elif not has_duplicates(last_four):
                     return i+1
-                else:
-                    last_four.append(c)
-            else: 
-                if c in last_four: 
-                    cycleArray(last_four, c)
-                else:
+
+def part2():
+    last_fourteen = []
+    with open("prob6input.txt") as file: 
+        string = file.read().strip()
+        for i in range(0,len(string)-1): 
+            c = string[i]
+            if(len(last_fourteen) < 14):
+                last_fourteen.append(c)
+            elif has_duplicates(last_fourteen): 
+                last_fourteen = cycleArray(last_fourteen, c)
+                if i == 15 : 
+                    print(last_fourteen)
+                elif not has_duplicates(last_fourteen):
                     return i+1
+
 
 def cycleArray(arr, append):
     arr.append(append)
     arr = arr[1:]
+    return arr
 
-print(main())
+def has_duplicates(list): 
+    return len(list) != len(set(list))
+
+print(part1())
+print(part2())
